238704883e8bf2f41d7201a6ffef81d82eb9b6ab
Andrei Kavalikhin <andrei_kavalikhin@epam.com>
2023-02-23 19:46:16 +0400

    Revert "Feat and style new endpoints and logging."
    This reverts commit 565c084ae6fbf6778b396abfe779e01e9f682cb2.

--- src/main/java/com/cit/util/CSVReader.java
+++ src/main/java/com/cit/util/CSVReader.java
@@ -1,27 +1,21 @@
 package com.cit.util;
 
-import com.cit.configuration.DBUpdate;
 import com.cit.entity.CryptoCurrency;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class CSVReader implements Reader {
-    private final List<Path> filePaths;
-    Logger logger = LoggerFactory.getLogger(CSVReader.class);
+    private List<Path> filePaths;
 
-    public CSVReader(File[] files) {
-        this.filePaths = Arrays.stream(files).map(File::toPath).toList();
+    public CSVReader(List<Path> filePaths) {
+        this.filePaths = filePaths;
     }
 
     @Override
@@ -29,14 +23,12 @@ public class CSVReader implements Reader {
         List<CryptoCurrency> data = new ArrayList<>();
         for (Path filePath : filePaths) {
             try (BufferedReader reader = Files.newBufferedReader(filePath)) {
-                logger.info(String.format("Parsing %s file",filePath.getFileName()));
-                CsvToBean<CryptoCurrency> cb = new CsvToBeanBuilder(reader)
+                CsvToBean<CryptoCurrency> cb = new CsvToBeanBuilder<CryptoCurrency>(reader)
                         .withType(CryptoCurrency.class)
-                        .withSeparator(',')
+                        .withSkipLines(1)
                         .build();
-                List<CryptoCurrency> parsedObjects = cb.parse();
-                logger.info(String.format("Parsed %d rows",parsedObjects.size()));
-                data.addAll(parsedObjects);
+
+                data.addAll(cb.parse());
             } catch (IOException e) {
                 e.printStackTrace();
             }
