565c084ae6fbf6778b396abfe779e01e9f682cb2
Andrei Kavalikhin <andrei_kavalikhin@epam.com>
2023-02-23 19:45:53 +0400

    Feat and style new endpoints and logging.
    Added new endpoints due to requirements and added logging for more
    convenient viewing of app flow

--- src/main/java/com/cit/CryptoInvestmentToolApplication.java
+++ src/main/java/com/cit/CryptoInvestmentToolApplication.java
@@ -2,12 +2,18 @@ package com.cit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@ComponentScan
+@EntityScan
 public class CryptoInvestmentToolApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CryptoInvestmentToolApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoInvestmentToolApplication.class, args);
+    }
 
 }
